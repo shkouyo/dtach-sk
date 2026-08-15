149f9651d423bf9b95506578698e4b2a034a023d
ShinKouyo <i@0x0f.dev>
2026-08-16 03:32:58 +0800

    chore: add copyright notice to C sources
    Co-Authored-By: Pi <https://pi.dev/>

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /*
     dtach-sk
     Copyright (C) 2004-2016 Ned T. Crigler
+    Copyright (C) 2026 ShinKouyo <i@0x0f.dev>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
